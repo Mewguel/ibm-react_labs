668e34af356c2e2c3789211344c22544f331e914
Ivan Miguel Malabanan <meguelvan@gmail.com>
2024-03-06 05:19:19 +0800

    [Update] Finished todo app

--- todo_list/src/App.js
+++ todo_list/src/App.js
@@ -1,28 +1,134 @@
-import React, {useState} from "react";
+import React, {useState,useEffect} from "react";
 import "./App.css";
+
 const App = () => {
   const [todos, setTodos] = useState([]);
+  const [todoEditing, setTodoEditing] = useState(null);
+
+  useEffect(() => {
+    const json = localStorage.getItem("todos");
+    const loadedTodos = JSON.parse(json);
+    if (loadedTodos) {
+      setTodos(loadedTodos);
+    }
+  }, []);
+
+  useEffect(() => {
+    if(todos.length > 0) {
+        const json = JSON.stringify(todos);
+        localStorage.setItem("todos", json);
+    }
+  }, [todos]);
   
   // Add the handlesubmit code here
-  
+  const handlesubmit = (e) => {
+    e.preventDefault();
+
+    let todo = document.getElementById('todoAdd').value;
+    const newTodo = {
+      id: new Date().getTime(),
+      text: todo.trim(),
+      completed: false,
+    }
+
+    if(newTodo.text.length > 0 ){
+      setTodos([...todos].concat(newTodo));
+    } 
+    else {
+      alert("Enter Valid Task!");
+    }
+
+    document.getElementById('todoAdd').value = "";
+  }
   
   // Add the deleteToDo code here
-
+  const deleteTodo = (id) => {
+    let updatedTodoList = [...todos].filter((todo) => todo.id != id);
+    setTodos(updatedTodoList);
+  }
   
   // Add the toggleComplete code here
+  const toggleComplete = (id) => {
+    let updatedTodoList = [...todos].map( (todo) => {
+      if(todo.id === id){
+        todo.completed = !todo.completed;
+      }
+      return todo;
+    });
 
-  
+    setTodos(updatedTodoList);
+
+  }
+
   // Add the submitEdits code here
+  const submitEdits = (newTodo) => {
+    if(null === newTodo) return;
+    
+    const updatedTodos = [...todos].map((todo) => {
+      if (todo.id === newTodo.id) {
+        todo.text = document.getElementById(newTodo.id).value;
+        }
+        return todo;
+      });
+    setTodos(updatedTodos);
+    setTodoEditing(null);
 
+  }
   
-return(
-<div className ="App">
-<h1>Todo List</h1>
-<form>
-<input type ="text" align ="right" id= 'todoAdd'/>
-<button type ="submit">Add Todo</button>
-</form>
-</div>
-);
+  return(
+    <div className ="todo-list">
+      <h1>Todo List</h1>
+      <form onSubmit={handlesubmit}>
+        <input 
+          type ="text" 
+          align ="right" 
+          id= 'todoAdd'/>
+        <button type ="submit">Add Todo</button>
+      </form>
+      {
+        todos.map( (todo) => 
+          <div className="todo" key={todo.id}>
+          <div className="todo-text">
+              {/* Add checkbox for toggle complete */}
+              <input
+                type="checkbox"
+                id="completed"
+                checked={todo.completed}
+                onChange={() => toggleComplete(todo.id)}
+              />
+              {/* if it is edit mode, display input box, else display text */}
+              {todo.id === todoEditing ?
+                (<input
+                  type="text"
+                  id = {todo.id}
+                  defaultValue={todo.text}
+                />) :
+                (<div>{todo.text}</div>)
+              }
+            </div>
+            <div className="todo-actions">
+              {/* if it is edit mode, allow submit edit, else allow edit */}
+              {todo.id === todoEditing ?
+              (
+                <button onClick={() => submitEdits(todo)}>Submit Edits</button>
+              ) :
+              (
+                <button onClick={() => setTodoEditing(todo.id)}>Edit</button>
+              )}
+              <button onClick={() => deleteTodo(todo.id)} 
+                      style={{ width: '120px', 
+                                backgroundColor: 'blue', 
+                                color: 'white', 
+                                border: 'none', 
+                                padding: '8px 12px', 
+                                cursor: 'pointer' }}>
+                        Delete
+              </button>
+             </div>
+          </div>
+        )
+      }
+    </div>
+  );
 };
 export default App;
